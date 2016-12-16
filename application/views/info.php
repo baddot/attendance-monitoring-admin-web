6fff51243a3a081f89ade03bc8011c0106d7e998
lloric <lloric@lloric>
2016-12-16 16:59:28 +0800

    ...

---
+++ application/views/info.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <?php echo $month . ' ' . $year; ?>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->	
---
+++ application/views/total.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <?php echo '<h3>Teacher: ' . $fullname . '</h3>'; ?>
+                <?php echo '<h4>Total Present: ' . $total . '</h4>' ?>
+                <?php echo '<h4>Total Absent: ' . $totalabsent . '</h4>' ?>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->	
